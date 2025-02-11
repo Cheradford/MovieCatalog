1fec94cc3bd955c379677ffcea16f334c7e76e2c
cheradford <dolgikh2211@gmail.com>
2025-02-11 03:38:55 +0300

    App running on 8080 port

--- MovieCatalog.View/Program.cs
+++ MovieCatalog.View/Program.cs
@@ -8,18 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton(new Manager());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");// Открыть Swagger на корне
+});
+
+
+/*app.UseHttpsRedirection();*/
 
 app.UseAuthorization();
 app.UseHttpMetrics();
